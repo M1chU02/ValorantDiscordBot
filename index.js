2a38a8e7b3b7eac80dd60484ce44ddfea36b4554
M1chU02 <m.mikolajewski02@gmail.com>
2025-06-05 14:36:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+import {
+  Client,
+  GatewayIntentBits,
+  REST,
+  Routes,
+  SlashCommandBuilder,
+  Events,
+} from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+].map((command) => command.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: commands,
+    });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
